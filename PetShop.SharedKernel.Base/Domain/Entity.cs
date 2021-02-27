1d18d19b5f428a603b4382495a05b9e8465a6544
Ricardo Augusto Vicentini <ric_vicentini@hotmail.com>
2021-02-27 08:21:31 -0300

    atualizações na entidade base

--- PetShop.SharedKernel.Base/Domain/Entity.cs
+++ PetShop.SharedKernel.Base/Domain/Entity.cs
@@ -6,7 +6,7 @@ namespace PetShop.SharedKernel.Base.Domain
 {
     public abstract class Entity
     {
-        int? _requestedHashCode;
+        
         public Guid Id { get; private set; }
 
         public Entity()
@@ -16,33 +16,34 @@ namespace PetShop.SharedKernel.Base.Domain
 
         public override bool Equals(object obj)
         {
-            if (obj == null || !(obj is Entity))
+            var other = obj as Entity;
+
+            if (ReferenceEquals(other, null))
                 return false;
-            if (Object.ReferenceEquals(this, obj))
+                
+            if (Object.ReferenceEquals(this, other))
                 return true;
-            if (this.GetType() != obj.GetType())
+
+            if (this.GetType() != other.GetType())
                 return false;
 
-            Entity item = (Entity)obj;
-
-            return item.Id == this.Id;
+            return other.Id == this.Id;
         }
 
         public override int GetHashCode()
         {
-            if (!_requestedHashCode.HasValue)
-                _requestedHashCode = this.Id.GetHashCode() ^ 31;
-            // XOR for random distribution. See:
-            // https://docs.microsoft.com/archive/blogs/ericlippert/guidelines-and-rules-for-gethashcode
-            return _requestedHashCode.Value;
+            return (GetType().ToString() + Id.ToString()).GetHashCode();
         }
 
         public static bool operator ==(Entity left, Entity right)
         {
-            if (Object.Equals(left, null))
-                return (object.Equals(right, null));
-            else
-                return left.Equals(right);
+            if (ReferenceEquals(left, null) && ReferenceEquals(right, null))
+                return true;
+
+            if (ReferenceEquals(left, null) || ReferenceEquals(right, null))
+                return false;
+
+            return left.Equals(right);
         }
 
         public static bool operator !=(Entity left, Entity right)
